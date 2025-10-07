1d1739bb48b5656a4385f29072a593a4a1ad7476
Randy <74595594+randy-ariesta-muliawan@users.noreply.github.com>
2025-10-07 12:06:54 +0700

    Add files via upload

---
+++ 00_Procedural/03_invisivle_character.py
@@ -0,0 +1,8 @@
+print("Ada dua orang anak, namanya Andi dan Budi.\nMereka berdua adalah teman dekat.\nHari ini mereka pergi mendaki gunung bersama.")
+print("Nama\t :")
+print("Kelas\t :")
+
+# Invisible Character :
+# Enter   : \n
+# Spasi   : 
+# Tabbing : \t
