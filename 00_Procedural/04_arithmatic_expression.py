9ac97074105f3bf53cfee320069904c6d5458552
Randy <74595594+randy-ariesta-muliawan@users.noreply.github.com>
2025-10-08 13:48:12 +0700

    Add files via upload

---
+++ 00_Procedural/04_arithmatic_expression.py
@@ -0,0 +1,19 @@
+print("2 + 2 = ", 2+2)
+print("3 * 4 = ", 3*4)
+print("100 - 1 = ", 100-1)
+print("(33+2)/5 + 10 = ", (33+2)/5 + 10)
+
+# Operasi Matematika
+# + - * /
+# ** (pangkat)
+print("3**2 = ", 3**2)
+# % (modulo)
+"""
+5 % 2 = 2 sisa 1 = 1
+10 % 3 = 3 sisa 1 = 1
+100 % 7 = 14 sisa 2 = 2
+"""
+print("14%3 =", 14%3)
+
+# Integer division (//)
+print("14//3", 14//3)
