d80648bab0ff19b92fd1c0ce82f9a62739488f5e
Randy <74595594+randy-ariesta-muliawan@users.noreply.github.com>
2025-10-12 18:32:34 +0700

    Add files via upload

---
+++ 00_Procedural/07_kalkulator_tambah.py
@@ -0,0 +1,10 @@
+a = int(input("Masukkan angka pertama	: "))
+b = int(input("Masukkan angka kedua	: "))
+print(a+b)
+
+"""
+TIPE DATA
+integer = bilangan bulat = int
+float = bilangan koma
+string = rangkaian huruf = ("")
+"""
