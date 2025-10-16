40c948cf9b0266a6a5bab2d61320560835bc3b97
Randy <74595594+randy-ariesta-muliawan@users.noreply.github.com>
2025-10-16 16:28:38 +0700

    Add files via upload

---
+++ 00_Procedural/10_sum_loop.py
@@ -0,0 +1,19 @@
+angka = 1
+jumlah = 0
+print("Masukkan angka baru ke dalam jumlah !")
+print("Masukkan angka 0 untuk keluar")
+while angka != 0 :
+	print("Jumlah angka sekarang", jumlah)
+	angka = float(input("Masukkan angka : "))
+	jumlah += angka
+print("Jumlah angka = ", jumlah)
+
+# Ekspresi Matematika
+"""
+== (sama dengan)
+!= (tidak sama dengan)
+> (lebih besar)
+>= (lebih besar sama dengan)
+< (lebih kecil)
+<=(lebih kecil sama dengan)
+"""
