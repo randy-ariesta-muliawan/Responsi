815c7b9d3d5f94ac9c9bb031152f010460be0fb4
Randy <74595594+randy-ariesta-muliawan@users.noreply.github.com>
2025-10-18 11:35:51 +0700

    Add files via upload

---
+++ 00_Procedural/13_data_casting.py
@@ -0,0 +1,32 @@
+# Casting ke string
+a = "abc"
+b = 23
+c = 3.5
+
+# str()
+b = str(b)
+c = str(c)
+print(a+b+c)
+
+# Casting ke integer
+a = "12"
+b = 23
+c = 3.5
+
+# int()
+a = int(a)
+b = int(b)
+c = int(c)
+print(a+b+c)
+
+# Casting ke float
+a = "12"
+b = 23
+c = 3.5
+# float()
+a = float(a)
+b = float(b)
+c = float(c)
+print(a+b+c)
+
+# bool() list() dict(), dsb
