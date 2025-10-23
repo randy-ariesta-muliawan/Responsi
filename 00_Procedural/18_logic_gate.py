2f737ad0556d639303077335426ac9ca36f8487a
Randy <74595594+randy-ariesta-muliawan@users.noreply.github.com>
2025-10-23 07:17:31 +0700

    Add files via upload

---
+++ 00_Procedural/18_logic_gate.py
@@ -0,0 +1,23 @@
+"""
+Boolean :
+- True (benar)
+- False (salah)
+
+Logic Gate :
+- and (dan)
+- or (atau)
+- not (tidak)
+"""
+
+print("True dan True", True and True)
+print("True dan False", True and False)
+print("False dan True", False and True)
+print("False dan False", False and False)
+
+print("True atau True", True or True)
+print("True atau False", True or False)
+print("False atau True", False or True)
+print("False atau False", False or False)
+
+print("Not True :", not True)
+print("Not False : ", not False)
