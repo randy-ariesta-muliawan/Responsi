aba48175a657dee8235607029e7df022300581cd
Randy <74595594+randy-ariesta-muliawan@users.noreply.github.com>
2025-11-02 18:46:27 +0700

    Add files via upload

---
+++ 00_Procedural/28_review.py
@@ -0,0 +1,15 @@
+# a = 10
+# def buat():
+# 	a = 20
+# 	b = 10
+# 	print(f"Didalam function, a = {a}, b = {b}")
+
+# buat()
+# print(f"Diluar function, a = {a}, b = {b}")
+
+def tambah(a, b):
+	hasil = a + b
+	return hasil
+
+jumlah = tambah(1,2)
+print(jumlah)
