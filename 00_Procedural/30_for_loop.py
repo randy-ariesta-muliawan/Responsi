8fc3a7d5d4d85e83ca54afbac66845e1ce5d5758
Randy <74595594+randy-ariesta-muliawan@users.noreply.github.com>
2025-11-04 08:48:10 +0700

    Add files via upload

---
+++ 00_Procedural/30_for_loop.py
@@ -0,0 +1,11 @@
+makanan = ["burger", "pempek", "permen", "tekwan", "nasi"]
+
+# for item in makanan :
+# 	print(item)
+
+# sampai10 = list(range(1,10))
+# print(sampai10)
+# sampai = range(a, b)
+# a <= sampai10 <= 10
+for angka in range(10, 1, -1) : # awal (0), akhir, perubahan(+1)
+	print(angka)
