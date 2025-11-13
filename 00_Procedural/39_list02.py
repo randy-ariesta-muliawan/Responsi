0ebe1368146f72c7d79cef549e8f08b668e94294
Randy <74595594+randy-ariesta-muliawan@users.noreply.github.com>
2025-11-13 12:50:35 +0700

    Add files via upload

---
+++ 00_Procedural/39_list02.py
@@ -0,0 +1,10 @@
+makanan_favorit = ["pempek", "permen", "tekwan"]
+favorit_teman = makanan_favorit[0:2] # [:] copy
+
+print(makanan_favorit)
+print(favorit_teman)
+
+makanan_favorit.append("kue")
+
+print(makanan_favorit)
+print(favorit_teman)
