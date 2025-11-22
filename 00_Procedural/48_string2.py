ec866960f61667d933aecb8f8c7769a9bdb4a99c
Randy <74595594+randy-ariesta-muliawan@users.noreply.github.com>
2025-11-22 08:04:00 +0700

    Add files via upload

---
+++ 00_Procedural/48_string2.py
@@ -0,0 +1,18 @@
+# nama = ["R", "a", "n", "d", "y"]
+# nama2 = "Randy"
+# print(nama2[0:2])
+# print(f"{nama[0]}{nama[1]}")
+
+# print(nama2)
+# for karakter in nama:
+# 	print(karakter, end="")
+
+#ASCII
+# ord -> melihat kode(angka) sebuah karakter
+# chr() -> melihat karakter dari sebuah kode ASCII
+
+# print(ord("A"))
+print("abcde...z")
+
+for i in range(26)
+	print(chr(97+i))
