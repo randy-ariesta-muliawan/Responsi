ead3d45b424a177f8458be5f93fdea4d2131f141
Randy <74595594+randy-ariesta-muliawan@users.noreply.github.com>
2025-12-08 11:30:48 +0700

    Add files via upload

---
+++ random_dice_generator/settings.py
@@ -0,0 +1,7 @@
+class Settings:
+
+	def __init__(self):
+		self.app_title = "Roll The Dice"
+		self.app_geometry = "450x450"
+
+		self.dice_path = ["img/dadu-1.png", "img/dadu-2.png", "img/dadu-3.png", "img/dadu-4.png", "img/dadu-5.png", "img/dadu-6.png"]
